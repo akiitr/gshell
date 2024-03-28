ec4389cf81084bcea7a3babee10649a9fa0ef0b2
Anubhav Kumar <55649509+akiitr@users.noreply.github.com>
2024-03-28 20:29:18 +0530

    Update start_gui.sh
    added command to remove the installed pkgs

--- start_gui.sh
+++ start_gui.sh
@@ -5,14 +5,14 @@ rm -rf chrome-remote-desktop_current_amd64.deb
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
 
-#Google Chrome browser:
+Google Chrome browser:
 rm -rf google-chrome-stable_current_amd64.deb
-https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install --assume-yes ./direct/google-chrome-stable_current_amd64.deb
 
 #utility packages
 sudo apt-get install --assume-yes vim-gtk3
-sudo apt-get install --assume-yes vim-gtk3
+sudo apt-get install --assume-yes gnome-terminal
 
 #XFCE Desktop Lite Environment:
 sudo DEBIAN_FRONTEND=noninteractive \
@@ -21,4 +21,9 @@ sudo DEBIAN_FRONTEND=noninteractive \
 #Start the GUI session
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo systemctl disable lightdm.service
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHDr0YbJrJbOUPUY1fheM3OuNK2P_qFM57fL0fWu7GdvKrFpfcLs10sU89wFV7GgBw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+#To start the chrome connection (update: you need to get that for every run from chrome remote desktop from setup via SSH by selecting as debian
+#DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHDr0YbJrJbOUPUY1fheM3OuNK2P_qFM57fL0fWu7GdvKrFpfcLs10sU89wFV7GgBw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+
+#Remove the installed pkgs
+rm -rf chrome-remote-desktop_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
