ec5842d907c8b7617debcea4a091f5c3964e7570
akiitr <akumar14@ec.iitr.ac.in>
2024-03-30 04:30:19 +0530

    fixed the issue of google remote gui connection not working asking for update

--- wsl.sh
+++ wsl.sh
@@ -10,6 +10,10 @@ sudo rm /etc/resolv.conf
 sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
 sudo bash -c 'echo "[network]" > /etc/wsl.conf'
 sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+# This is to resolve the issue of remote desktop not connecting showinf for update
+sudo bash -c 'echo "[boot]" > /etc/wsl.conf'
+sudo bash -c 'echo "systemd=true" >> /etc/wsl.conf'
+
 sudo chattr +i /etc/resolv.conf
 
 # update the env
